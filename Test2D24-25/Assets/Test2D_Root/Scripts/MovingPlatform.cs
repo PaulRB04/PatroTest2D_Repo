63b35b9c9a0b2752581a7d8b48e3fd3153b09376
Marcfzv <marcferrandiz2005@gmail.com>
2024-12-18 19:51:04 +0100

    Añadida cámara y demas

---
+++ Test2D24-25/Assets/Test2D_Root/Scripts/MovingPlatform.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] float speed = 5; //Velociad de la plataforma
+    [SerializeField] int startingPoint; //Determinador del punto de inicio de la plataforma
+    [SerializeField] Transform[] points; //Array que almacena la posición de los diferentes puntos de alcance
+    int i; //Indice del array = Punto que va a perseguir la plataforma
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Al inicio del juego la plataforma se teleporta a la posición de igual vvalor que startingPoint
+        transform.position = points[startingPoint].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MovePlatform();
+
+    }
+
+    void MovePlatform()
+    {
+        if(Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        {
+            i++; //Suma 1 al valor del indice, persigue el siguiente punto
+            if (i == points.Length) i = 0; //Resetea el circuito de puntos
+        }
+
+        //Mueve la plataforma a la posicion del punto en el que el array coincida con el valor del indice
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+}
